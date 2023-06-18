cfc1523f1088f7855f76172e7506a7ddddd37426
Siruenyian <92420947+Siruenyian@users.noreply.github.com>
2023-06-18 23:54:06 +0700

    feat: add order, product repo interface

---
+++ app/Core/Repository/OrderRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Core\Repository;
+
+use App\Core\Models\Order\Order;
+use App\Core\Models\Order\OrderId;
+
+interface OrderRepositoryInterface
+{
+    public function byId(OrderId $id): ?Order;
+    public function save(Order $order): void;
+    public function update(Order $order): void;
+}
---
+++ app/Core/Repository/ProductRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Core\Repository;
+
+use App\Core\Models\Product\Product;
+use App\Core\Models\Product\ProductId;
+
+interface ProductRepositoryInterface
+{
+    public function byId(ProductId $id): ?Product;
+    public function save(Product $product): void;
+    public function update(Product $product): void;
+    public function delete(Product $product): void;
+}
