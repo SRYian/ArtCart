73808ac1ef8b76c04d24c2ad925f464faa6fe727
Siruenyian <92420947+Siruenyian@users.noreply.github.com>
2023-06-21 22:14:09 +0700

    feat: fix coupon repository

--- app/Infrastructure/Repository/MySQL/CouponRepository.php
+++ app/Infrastructure/Repository/MySQL/CouponRepository.php
@@ -3,21 +3,21 @@
 namespace App\Infrastructure\Repository\MySQL;
 
 use App\Core\Models\Coupon\Coupon;
-use App\Core\Models\Coupon\CouponId;
+use App\Core\Models\Coupon\CouponCode;
 use App\Core\Repository\CouponRepositoryInterface;
 use Illuminate\Support\Facades\DB;
 
 class CouponRepository implements CouponRepositoryInterface
 {
 
-    public function byId(CouponId $id): ?Coupon
+    public function byId(CouponCode $code): ?Coupon
     {
 
-        $row = DB::table('coupon')->where('coupon_code', $id->id())->first();
-        if(!$row) return null;
+        $row = DB::table('coupon')->where('coupon_code', $code->getCode())->first();
+        if (!$row) return null;
 
         return new Coupon(
-            $id,
+            $code,
             $row->user_id,
             $row->discount,
             $row->description,
@@ -28,6 +28,7 @@ class CouponRepository implements CouponRepositoryInterface
     public function save(Coupon $coupon): void
     {
         $payload = $this->constructPayload($coupon);
+        $payload['user_id'] = $coupon->getUserId()->id();
         DB::table('coupon')->insert($payload);
     }
 
@@ -35,18 +36,19 @@ class CouponRepository implements CouponRepositoryInterface
     {
         $payload = $this->constructPayload($coupon);
         DB::table('coupon')
-            ->where('coupon_code', $coupon->getCouponCode())
+            ->where('coupon_code', $coupon->getCouponCode()->getCode())
             ->update($payload);
     }
 
     private function constructPayload(Coupon $coupon)
     {
+        // TODO: sesuiin sama Db
         return [
-            "coupon_code" => $coupon->getCouponCode(),
             "user_id" => $coupon->getUserId(),
             "discount" => $coupon->getDiscount(),
-            "description" => $coupon->getDescription(),
-            "photourl" => $coupon->getPhotoUrl(),
+            "uses" => $coupon->getUses(),
+            "max_use" => $coupon->getMaxUse(),
+            "photourl" => $coupon->getBerlakuSampai(),
         ];
     }
 }
