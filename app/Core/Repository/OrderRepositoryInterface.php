21966f5d60694d44ac50509df06d0a2ff75ee54e
Siruenyian <92420947+Siruenyian@users.noreply.github.com>
2023-06-21 23:17:45 +0700

    feat: update show method implementation

--- app/Core/Repository/OrderRepositoryInterface.php
+++ app/Core/Repository/OrderRepositoryInterface.php
@@ -4,11 +4,12 @@ namespace App\Core\Repository;
 
 use App\Core\Models\Order\Order;
 use App\Core\Models\Order\OrderId;
+use App\Core\Models\User\UserId;
 
 interface OrderRepositoryInterface
 {
     public function byId(OrderId $id): ?Order;
-    public function show(): array;
+    public function show(UserId $id): array;
     public function save(Order $order): void;
     public function update(Order $order): void;
 }
