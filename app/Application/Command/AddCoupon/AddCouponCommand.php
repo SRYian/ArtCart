b6a1168d7b7336ce27c029894d4949846b7574b0
Siruenyian <92420947+Siruenyian@users.noreply.github.com>
2023-06-21 22:14:31 +0700

    feat: add Addcoupon commands

--- app/Application/Command/AddCoupon/AddCouponCommand.php
+++ app/Application/Command/AddCoupon/AddCouponCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Application\Command\AddCoupon;
 
 use App\Core\Models\Coupon\Coupon;
+use App\Core\Models\Coupon\CouponCode;
 use App\Core\Models\Coupon\CouponId;
 use App\Core\Models\Coupon\DescriptionCoupon;
 use App\Core\Models\Coupon\Discount;
@@ -23,17 +24,23 @@ class AddCouponCommand
 {
     public function __construct(
         private CouponRepositoryInterface $couponRepository
-    ) { }
+    ) {
+    }
 
     public function execute(AddCouponRequest $request): void
     {
+        // public string $couponCode,
+        // public string $userId,
+        // public float $discount,
+        // public string $description,
+        // public DateTime $berlakuSampai,
+        $coupon = $this->couponRepository->byId(new CouponCode($request->couponCode));
+        if ($coupon) {
+            throw new InvalidArgumentException('coupon_sudah_ada');
+        }
         DB::beginTransaction();
         try {
-            $coupon = addCoupon(
-                new Discount($request->discount),
-                new DescriptionCoupon($request->description),
-                new PhotoUrlCoupon($request->photourl)
-            );
+            $coupon = new Coupon(new CouponCode($request->couponCode), new UserId($request->userId), $request->discount, $request->maxUse, $request->berlakuSampai);
             $this->couponRepository->save($coupon);
         } catch (Throwable $exception) {
             DB::rollBack();
