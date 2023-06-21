e0ea602c8cf86e7e1fba4e2a19f13fabc8c09355
Siruenyian <92420947+Siruenyian@users.noreply.github.com>
2023-06-21 22:15:05 +0700

    feat: add Edit product commands

--- app/Application/Command/EditProduct/EditProductCommand.php
+++ app/Application/Command/EditProduct/EditProductCommand.php
@@ -3,6 +3,16 @@
 namespace App\Application\Command\EditProduct;
 
 use App\Core\Repository\ProductRepositoryInterface;
+use DateTime;
+use DateTimeZone;
+use Illuminate\Support\Facades\DB;
+use InvalidArgumentException;
+use Throwable;
+use function intval;
+use Ramsey\Uuid\Uuid;
+use App\Core\Models\Product\Product;
+use App\Core\Models\Product\ProductId;
+use App\Application\Exception\ApplicationException;
 
 class EditProductCommand
 {
@@ -17,6 +27,19 @@ class EditProductCommand
     }
     public function  execute(EditProductRequest $request)
     {
+        $product = $this->productRepository->byId(new ProductId($request->id));
+        if (!$product) {
+            throw new ApplicationException('produk_tidak_ditemukan');
+        }
         //TODO:create product edit request
+        DB::beginTransaction();
+        try {
+            $product->updateProduct($request->name, $request->price, $request->weight, $request->stock, $request->description, $request->photourl);
+            $this->productRepository->update($product);
+        } catch (Throwable $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
+        DB::commit();
     }
 }
--- app/Application/Command/EditProduct/EditProductRequest.php
+++ app/Application/Command/EditProduct/EditProductRequest.php
@@ -5,11 +5,14 @@ namespace App\Application\Command\EditProduct;
 class EditProductRequest
 {
     public function __construct(
-        string $name,
-        int $price,
-        int $weight,
-        int $stock,
-        string $description
+        public string $id,
+        public string $name,
+        public int $price,
+        public int $weight,
+        public int $stock,
+        public string $description,
+        public string $photourl
+
     ) {
     }
 }
