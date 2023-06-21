bc313cc8eeb3454eef18096b81bd62eeb4b24d48
Siruenyian <92420947+Siruenyian@users.noreply.github.com>
2023-06-22 01:43:53 +0700

    feat: fix web named route

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/product/{id}', [ProductsController::class, 'index'])->name('product_detail');
 
