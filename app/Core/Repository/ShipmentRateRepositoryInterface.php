82fd3912cccba4830bfa274216e61eb76d553c0a
Hafizh Mufid <79137636+migorengrendang165@users.noreply.github.com>
2023-06-21 15:33:49 +0700

    Update ShipmentRateRepositoryInterface.php

--- app/Core/Repository/ShipmentRateRepositoryInterface.php
+++ app/Core/Repository/ShipmentRateRepositoryInterface.php
@@ -3,9 +3,9 @@
 namespace App\Core\Repository;
 
 use App\Core\Models\ShipmentRate;
-use App\Core\Models\Shipment;
+use App\Core\Models\ShipmentRateId;
 
 interface ShipmentRateRepositoryInterface
 {   
-    public function byId(ShipmentRate $id): ?ShipmentRate;
+    public function byId(ShipmentRateId $id): ?ShipmentRate;
 }
